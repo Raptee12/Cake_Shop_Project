29c7266940d5dbc1d52d63badbf7b539eb636df2
Raptee12 <rapteesengupta.jpg@gmail.com>
2023-06-29 19:58:51 +0530

    modify .

--- server.js
+++ server.js
@@ -24,16 +24,16 @@ app.use('/api/cakes', require('./routes/cakeRoute'))
 app.use('/api/users', require('./routes/userRoute'))
 app.use('/api/orders', require('./routes/orderRoute'))
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, '/client/build')))
-  app.get('*', (req,res) => {
-    res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+//static files
+  app.use(express.static(path.join(__dirname, './client/build')))
+  app.get('*', function(req,res) {
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
   })
-}else{
+
     app.get("/", (req,res) => {
         res.send('<h1> Hello from Node Server via nodemon</h1>')
     })
-}
+
 
 
 //port
